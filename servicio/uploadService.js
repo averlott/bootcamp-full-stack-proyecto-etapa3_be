e6ac4c7b169bd9831ec96d26aae1b18f8b514573
averlott <averlott@hotmail.com>
2025-12-02 03:32:28 -0300

    0.0.3

--- servicio/uploadService.js
+++ servicio/uploadService.js
@@ -3,7 +3,8 @@ class Servicio {
     constructor() {}
 
     recibirArchivo = async function(file) {
-        const url = 'http://localhost:8080/tmp/uploads/' + file.filename
+        //const url = 'http://localhost:8080/tmp/uploads/' + file.filename
+        const url = './tmp/uploads/' + file.filename
         return url
     }
 }
